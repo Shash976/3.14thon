8371070b827baf3609eaf70ecb8d82f9fa3b2066
Shashwat Goel <itshashgoel@gmail.com>
2023-10-14 12:41:23 +0530

    switched from music_tag to eyed3a

--- download_mp3.py
+++ download_mp3.py
@@ -1,7 +1,7 @@
 import pytube
 import os
 import sys
-import music_tag
+import eyed3
 
 def getUrls(playlist_url):
     urls = []
@@ -12,29 +12,28 @@ def getUrls(playlist_url):
     
     return urls, playlist_name
 
-def getMP3fromLink(video_link, playlist_name = ""):
+def getMP3fromLink(video_link, playlist_name = "", destination=r"C:\Users\shash\Downloads\prisha"):
     youtube_vid = pytube.YouTube(video_link)
     video = youtube_vid.streams.filter(only_audio=True).first()
-
-    destination = r"C:\Users\shashg\Downloads\prisha"
-
     out_file = video.download(output_path=destination)
-
     base,ext = os.path.splitext(out_file)
     new_file = base + '.mp3'
     os.rename(out_file,new_file)
-    track = music_tag.load_file(new_file)
-    track['title'] = youtube_vid.title
-    track['artist'] = youtube_vid.author.replace('VEVO', '')
-    track['album artist'] = youtube_vid.author.replace('VEVO', '')
-    track["album"] = playlist_name
-    track.save()
+    try:
+        track = eyed3.load(new_file)
+        track.initTag()
+        track.tag.title = youtube_vid.title
+        track.tag.artist = youtube_vid.author.replace('VEVO', '')
+        track.tag.album = playlist_name 
+        track.tag.save(version=(1,None,None))
+        track.tag.save()
+    except:
+        print("\tNo metadata found")
     print(youtube_vid.title + " is downloaded")
 
-def getMP4fromLink(video_link):
+def getMP4fromLink(video_link, SAVE_PATH=r"C:\shashg\Downloads\test_mp4"):
     yt = pytube.YouTube(video_link)
     mp4files = yt.filter('mp4') 
-    SAVE_PATH = r"C:\shashg\Downloads\test_mp4"
     # get the video with the extension and
     # resolution passed in the get() function 
     d_video = yt.get(mp4files[-1].extension,mp4files[-1].resolution)
@@ -43,7 +42,12 @@ def getMP4fromLink(video_link):
 
 if __name__ == '__main__' :
     playlist_url = sys.argv[1]
+    try:
+        if os.path.exists(sys.argv[2].strip()):
+            destination = sys.argv[2].strip()
+    except:
+        destination = r"C:\Users\shash\Downloads\prish"
     urls, playlist_name = getUrls(playlist_url)
     for url in urls:
-        getMP3fromLink(url, playlist_name)
+        getMP3fromLink(url, playlist_name, destination)
     print("Done")
